98b136507fa5a104e9fafa274ec91aa7d2d16c10
ivonnesue <ivonnesue12@gmail.com>
2023-04-22 21:50:15 -0700

    modified margins

--- scripts/mainportal.js
+++ scripts/mainportal.js
@@ -3,5 +3,5 @@ const currentDate = new Date();
 document.querySelector("#year").textContent = currentDate.getFullYear();
 
 let lastUpdate = document.lastModified;
-document.getElementById("#updated").innerHTML= lastUpdate;
+document.getElementById("updated").innerHTML= lastUpdate;
   
