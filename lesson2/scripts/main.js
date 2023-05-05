db429f2e7d1d99911346f233bb93465a1d2fa4b8
ivonnesue <ivonnesue12@gmail.com>
2023-05-05 15:37:53 -0700

    modified

---
+++ lesson2/scripts/main.js
@@ -0,0 +1,7 @@
+// Main scripts for Assignment portal
+const currentDate = new Date();
+document.querySelector("#year").textContent = currentDate.getFullYear();
+
+let lastUpdate = document.lastModified;
+document.getElementById("updated").innerHTML= lastUpdate;
+  
