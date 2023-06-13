c2a1106ee3fba60045fd5c7889fe643ecee29f91
ivonnesue <ivonnesue12@gmail.com>
2023-06-12 21:31:09 -0700

    corrected scripts for Form date and time

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -26,12 +26,10 @@ document.querySelector("#year").textContent = currentDate.getFullYear();
 let lastUpdate = document.lastModified;
 document.getElementById("updated").innerHTML= lastUpdate;
   
+//Banner
 let banner = document.querySelector(".banner");
 
-
-
 let day = new Date().getDay();
-
 
 if (day == 1 || day == 2)
 {
@@ -43,3 +41,18 @@ const closeBtn = document.querySelector("#close-button");
 closeBtn.addEventListener("click", () => {
     banner.style.display = "none";
 });
+
+//Form date and time
+
+const FormCurrentDate = new Date();
+const month = currentDate.getMonth() + 1;
+  const dayofWeek = currentDate.getDate();
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+ 
+  
+  const formDateTime = `${month.toString().padStart(2, '0')}-${dayofWeek.toString().padStart(2, '0')}-${year} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+  
+  document.getElementById("dateAndTime").value = formDateTime;
+
