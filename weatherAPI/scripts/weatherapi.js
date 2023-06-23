a09c323091185bffab4c2cee8c45eade5ed4e4ee
ivonnesue <ivonnesue12@gmail.com>
2023-06-22 22:20:32 -0700

    add script display function

--- weatherAPI/scripts/weatherapi.js
+++ weatherAPI/scripts/weatherapi.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164s&units=imperial&appid=699464eaf210022dc808bbdc771f5299";
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&appid=699464eaf210022dc808bbdc771f5299";
 
 async function apiFetch() {
     try {
@@ -19,6 +19,18 @@ async function apiFetch() {
     }
   }
   
+  function displayResults(weatherData){
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  
+}
+
   apiFetch();
 
-  //function displayResults('weatherdata')
+ 
